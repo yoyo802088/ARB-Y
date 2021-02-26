e8cbc87c128ea7ed790dee00d0ebcbb89b000c87
Marielle Eusebio <mariellejde@gmail.com>
2021-02-25 20:18:43 -0800

    Subscribe & Publisher node now

--- catkin_ws/src/ar_pkg/src/ar_cntrl_car.cpp
+++ catkin_ws/src/ar_pkg/src/ar_cntrl_car.cpp
@@ -4,34 +4,43 @@
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "simple_car_cntrl");
+	ros::init(argc, argv, "ar_car_cntrl");
 	ros::NodeHandle n;
-    // subscribe to ar tag detection
+
     ros::Subscriber ar_sub = n.subscribe("ar_pose_marker", 1000, poseCallback);
-    // publish to car servos (motor, steering)
 	ros::Publisher servo_pub = n.advertise<i2cpwm_board::ServoArray>("servos_absolute", 1000);
+
+    ar_track_alvar_msgs::AlvarMarkers markers;
+    i2cpwm_board::ServoArray msg;
+
+    // assume marker position
+    z = markers.markers[0].pose.pose.position.z;
+    z = 0;
+
+    // condition for how close cam should be to ar tag - arbitrary 0.5 value
+    double close = 0.5;
+
+    // set steering (servo 1) and throttle (servo 2)
+    servo = msg.servo; 
+    value = msg.value;
+
+    // assuming 300 is neutral for both - change if necessary
+    double neutral = 300.0; 
 	
-    // this while statement should check for a different condition
-    // like if the ar tag is x distance away ?
-    // not sure how to do this without both subscribing and publishing... hm
-    // something about the z value being small (meaning its very close to the camera)
-    // backup if z value reaches this point?
+    // Running at 10Hz - an arbitrary rate 
+	ros::Rate loop_rate(10);
+
     while(ros::ok())
     {
-        i2cpwm_board::ServoArray msg;
+        // if z is close then stop & reverse
+        if(z == close)){
+            neutral;
+            reverse;
+        }
 
-        // here i need to publish to servo value and its value
-        // servo 1 - steering
-        // servo 2 - throttle
-        // pwm limits (to test) - 300 neutral?
-
-        servo = msg.servo;
-        value = msg.value;
-
-        chatter_pub.publish(msg);
+        servo_pub.publish(something);
+        servo_pub.publish(something);
         ros::spinOnce();
-        loop_rate.sleep();
     }
-
 	return 0;
 }
